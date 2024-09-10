1693ec3d771390c73d2f4ef53704d20513e27a2e
SumanthChinivar <sumanthchinivar16@gmail.com>
2024-09-10 15:58:58 +0530

    Ultimate Push

--- TP01&02/src/collections/A1.java
+++ TP01&02/src/collections/A1.java
@@ -3,6 +3,7 @@ package collections;
 import java.util.ArrayList;
 
 public class A1 {
+	//added a file
 	public static void main(String[] args) {
 		ArrayList a=new ArrayList();//Object
 		a.add(10);
